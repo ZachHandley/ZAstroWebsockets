e2c714ef45a76f8857c3a5ae38897688021b22cf
GitHub Action <action@github.com>
2025-07-17 19:15:53 +0000

    release: zastro-websockets-cloudflare v12.6.0 for Astro 0.0.0

---
+++ packages/cloudflare/dist/websocket/middleware.js
@@ -0,0 +1,29 @@
+import { WebSocket, attach } from "./websocket.js";
+const onRequest = async function cloudflareWebSocketMiddleware(context, next) {
+  const { request, locals } = context;
+  const isUpgradeRequest = request.headers.get("upgrade") === "websocket" && request.headers.get("connection")?.toLowerCase().includes("upgrade");
+  locals.isUpgradeRequest = isUpgradeRequest;
+  locals.upgradeWebSocket = () => {
+    if (!isUpgradeRequest) {
+      throw new Error("The request must be an upgrade request to upgrade the connection to a WebSocket.");
+    }
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = [webSocketPair[0], webSocketPair[1]];
+    const socket = new WebSocket(request.url);
+    attach(socket, server);
+    const response = new Response(null, {
+      status: 101,
+      statusText: "Switching Protocols",
+      headers: {
+        "Upgrade": "websocket",
+        "Connection": "Upgrade"
+      },
+      webSocket: client
+    });
+    return { socket, response };
+  };
+  return next();
+};
+export {
+  onRequest
+};
