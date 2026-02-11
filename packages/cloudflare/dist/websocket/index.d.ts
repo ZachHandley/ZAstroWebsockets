bab2f5c56c771fce1728a2e7d603359e090e35b8
Forgejo CI <ci@forge.blackleafdigital.com>
2026-02-11 02:04:24 +0000

    release: zastro-websockets-cloudflare v12.6.12-4 for Astro 0.0.0

---
+++ packages/cloudflare/dist/websocket/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Cloudflare WebSocket exports
+ */
+export { WebSocket, attach, ErrorEvent, CloseEvent } from './websocket.js';
+export { onRequest } from './middleware.js';
+export { createWebSocketHandler } from './server.js';
